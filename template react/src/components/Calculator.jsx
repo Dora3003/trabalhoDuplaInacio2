1c15e4c632ca514afcc482dee90ff84b1056f7a2
Dora3003 <isadorajardim3003@gmail.com>
2025-10-03 08:15:20 -0300

    Colocando calculadora e alterado css

--- template react/src/components/Calculator.jsx
+++ template react/src/components/Calculator.jsx
@@ -1,7 +1,38 @@
 const Calculator = () => {
+    function setTextResult(result) {
+        const resultText = document.querySelector('.result')
+        resultText.innerHTML = `Resultado: ${result}`
+    }
+
+    const handleSum = (x, y) => {
+        setTextResult(x + y)
+    }
+
+    const handleSubtraction = (x, y) => {
+        setTextResult(x - y)
+    }
+
+    const handleMultiplication = (x, y, result) => {
+        setTextResult(x * y)
+    }
+
+    const handleDivision = (x, y, result) => {
+        setTextResult(x / y)
+    }
+
+    
+
     return (
         <div>
-            <h2>Calculadora Simples</h2>
+            <form action="">
+                <input type="number" id="n1" placeholder="Número 1" />
+                <input type="number" id="n2" placeholder="Número 2" />
+            </form>
+            <button onClick={() => handleSum(Number(n1.value), Number(n2.value))}>Somar ( + )</button>
+            <button onClick={() => handleSubtraction(Number(n1.value), Number(n2.value))}>Subtrair ( - )</button>
+            <button onClick={() => handleMultiplication(Number(n1.value), Number(n2.value))}>Multiplicar ( x )</button>
+            <button onClick={() => handleDivision(Number(n1.value), Number(n2.value))}>Dividir ( / )</button>
+            <p className="result">Resultado: </p>
         </div>
     )
 }
